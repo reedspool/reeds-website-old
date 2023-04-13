a801291317f35f1334fce143f1718120a3eda2ed
Reed Spool <reedwith2es@gmail.com>
2023-04-12 22:38:18 -0700

    Experimenting with bindProps

--- app/site/pages/blog/Layout.tsx
+++ app/site/pages/blog/Layout.tsx
@@ -7,6 +7,47 @@ import { DiscussionBox } from "../../renderer/DiscussionBox"
 import { PageContextExportDocumentProps, PageLayout } from "../../renderer/types"
 import { Case, Switch } from "./SwitchCase"
 import { Show } from "../../renderer/Show"
+import { ReactElement } from "react"
+
+export const RenderAll: React.FC<{ all: Array<ReactElement> }> = ({ all }) => <>{all}</>
+
+const bindProps:
+    <K>(fn: React.FC<K>, boundProps: Partial<K>) => React.FC<Partial<K>>
+    =
+    // This `as K` means that there is no type checking that you've got ALL
+    // the correct props, however there is type checking on the types of
+    // each prop, so that's a trade-off.
+    //
+    // One way to get around this challenge would be a
+    // third parameter for "defaults" which is NOT Partial<K> but just K,
+    // to guarantee that each prop is set to a correct value
+    <K,>(fn: React.FC<K>, boundProps: Partial<K>) => (props) => fn({ ...boundProps, ...props } as K);
+
+//@ts-ignore -- unused
+const bindPropsPermanent =
+    (fn: React.FC, boundProps: {}) =>
+        (props: {}, ...rest: Array<any>) =>
+            // The only difference is the order in which the props are spread
+            // All other errors are lack of updating this because I don't use it
+            fn({ ...props, ...boundProps, }, ...rest);
+
+/* Can we add this as a prototype to Function so that it can chain abritrarily? */
+declare global { interface Function { bindProps<K>(boundProps: K): ReturnType<typeof bindProps<K>> } }
+Function.prototype.bindProps = function <K>(boundProps: K) {
+    return bindProps(this as React.FC<K>, boundProps)
+}
+
+// This all allows:
+const BoundBlogFrontMatter = bindProps(BlogFrontMatter, { publishDate: "", updateDate: " " })
+const stuff: Array<ReactElement> = [];
+stuff.push(
+    <h1 className="mt-md">A little experiment</h1>,
+    <BoundBlogFrontMatter />,
+    <>{BlogFrontMatter.bindProps({ publishDate: "", updateDate: " " })}</>,
+    < BlogFrontMatter
+        publishDate={""}
+        updateDate={""} />);
+// Then return <RenderAll {...{ all: stuff }} />;
 
 export const Layout: PageLayout<PageContextExportDocumentProps> = ({ children, title, publishDate, addenda, discussionUrl }) => {
     const isAddenda = addenda && addenda.length > 0;
