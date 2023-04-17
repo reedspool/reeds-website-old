d19947757ca0d77c46b4f812bbed222c0bc0f03b
Human <reedwith2es@gmail.com>
2023-04-16 21:18:09 -0700

    feat: make Map component's map function optional

--- app/site/renderer/components/Map.tsx
+++ app/site/renderer/components/Map.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export type MapProps = {
     From: Array<{}>
-    To: FC<any>
+    To?: FC<any>
 }
 export const Map: FC<MapProps> =
-    ({ From, To }) => <>{From.map((props) => To(props))}</>
+    ({ From, To }) => <>{To ? From.map((props) => To(props)) : From}</>
